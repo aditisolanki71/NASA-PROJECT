a415aad92a462bc51793183914018428686cb99a
Aditi <adtsolanki22@gmail.com>
2023-05-06 11:56:05 +0530

    follow up deploy

--- server/src/app.js
+++ server/src/app.js
@@ -11,6 +11,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+//server public file
+// >npm run server
+//localhost:8000/
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
