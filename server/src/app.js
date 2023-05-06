6e679a06d3cf03c64a69d5ffa4dc044df78e07b1
Aditi <adtsolanki22@gmail.com>
2023-05-06 12:51:42 +0530

    Get planets Model-controller and UI

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 
 //2
 const planetsRouter = require('./routes/planets/planets.router')
+const launchesRouter = require('./routes/launches/launches.router')
 const app = express();
 
 app.use(cors({
@@ -17,6 +18,7 @@ app.use(morgan('combined'))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+app.use(launchesRouter);
 //server public file
 // >npm run server
 //localhost:8000/
---
+++ server/src/routes/launches/launches.router.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const { getAllLaunches } = require('../../routes/launches/launches.controller')
+const launchesRouter = express.Router();
+launchesRouter.get('/launches',getAllLaunches);
+
+module.exports = launchesRouter;
